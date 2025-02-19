6d96ec06df086884401d894544784c256e2a0a64
leiferksn <leiferksn@zoho.com>
2025-02-19 16:32:00 +0100

    add finite state machine

---
+++ state/src/main/java/tech/bouncystream/DocumentProcessor.java
@@ -0,0 +1,9 @@
+package tech.bouncystream;
+
+public class DocumentProcessor {
+
+    public static void main(final String args[]) {
+        final var doc = new Document();
+        doc.process();
+    }
+}
